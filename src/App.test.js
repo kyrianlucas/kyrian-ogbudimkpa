de9534b39186c7d6f80369abeea63284e3edc493
Kyrian Lucas <kyrianmax07@gmail.com>
2026-04-08 09:48:19 -0600

    Removed empty test file

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('shows my name on the page', () => {
-  render(<App />);
-  const nameElement = screen.getByText(/Kyrian Ogbudimkpa/i);
-  expect(nameElement).toBeInTheDocument();
-});
